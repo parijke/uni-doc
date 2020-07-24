3ded096a8920ccdf7baadc3a3527a759794c149d
Paul Rijke <paul@rijke.org>
2020-07-24 12:44:15 +0200

    first commit

---
+++ src/Controller/Admin/PostCrudController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Post;
+use App\Form\CommentType;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class PostCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Post::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('title'),
+            TextareaField::new('content'),
+            CollectionField::new('comments')
+                ->onlyOnForms()
+                ->allowAdd()
+                ->allowDelete()
+                ->setEntryIsComplex(true)
+                ->setEntryType(CommentType::class)
+                ->setFormTypeOptions([
+                    'by_reference' => false,
+                    'label' => false,
+                ]),
+
+        ];
+    }
+
+}
